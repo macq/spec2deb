caa31061a12063ef8e83ced37fea1a040dedd937
Vincent Raman <vincent.raman@macq.eu>
2015-09-20 08:47:54 +0200

    Don't build packages without file section

--- src/spec2deb/spec2deb.py
+++ src/spec2deb/spec2deb.py
@@ -1022,6 +1022,10 @@ class RpmSpecToDebianControl:
         yield "+Homepage: %s" % self.get("url", "")
         yield "+"
         for deb_package, package in sorted(self.deb_packages2()):
+            if not self.packages[package].get("%files"):
+                _log.warning(
+                    "Package %s doesn't have a %%files section, won't build", deb_package)
+                continue
             yield "+Package: %s" % deb_package
             group = self.packages[package].get("group", default_rpm_group)
             section = self.group2section(group)
